3b69b22bdd5087b991925e6b57c608e08235e98f
Ilya <iliark951@gmail.com>
2021-11-20 19:05:15 +0300

    update

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ def act(x):
     return 0 if x < 0 else 1
 
 
-def buy(comfort, price, brightcolored):
-    x = np.array([comfort, price, brightcolored])
+def buy(comfort, price, brightColored):
+    x = np.array([comfort, price, brightColored]) #вектор входных значений
     w11 = [0.8, 0.3, -1]
-    w12 = [0.3, 0, 2]
+    w12 = [0.3, 0, 2]     #веса на входах нейронов скрытого слоя
     weight1 = np.array([w11, w12])
-    weight2 = np.array([1, -1])
+    weight2 = np.array([1, -1]) #веса выходов нейронов скрытого слоя
 
-    sum_hidden = np.dot(weight1, x)  # вычисляем сумму на входах нейронов скрытого слоя
+    sum_hidden = np.dot(weight1, x)  #выполним скалярное произведение входных значений и значений весов на входах скрытого слоя
     print("Значения сумм на нейронах скрытого слоя: " + str(sum_hidden))
 
     out_hidden = np.array([act(x) for x in sum_hidden])
     print("Значения на выходах нейронов скрытого слоя: " + str(out_hidden))
 
     sum_end = np.dot(weight2, out_hidden)
-    y = act(sum_end)
+    y = act(sum_end)       #Воспользуемся функцией активации
     print("Выходное значение НС: " + str(y))
 
     return y
@@ -29,11 +29,10 @@ print("Вводите 1 для положительного ответа, 0 дл
 
 comfort = int(input("Салон комфортабельный?: "))
 price = int(input("Цена приемлемая?: "))
-brightcolored = int(input("Яркий цвет?: "))
+brightColored = int(input("Яркий цвет?: "))
 
 
-res = buy(comfort, price, brightcolored)
-if res == 1:
+if buy(comfort, price, brightColored) == 1:
     print("Машина подойдет для меня")
 else:
     print("Машина подойдет жене")
